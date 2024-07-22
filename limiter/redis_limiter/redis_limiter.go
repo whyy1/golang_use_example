d8c82d0a5b37048bddda567594ff5fa7cffd230b
whyy1 <1006307055@qq.com>
2024-07-22 16:43:19 +0800

    FIX:优化重试时间

--- limiter/redis_limiter/redis_limiter.go
+++ limiter/redis_limiter/redis_limiter.go
@@ -12,26 +12,28 @@ type RedisLimiter struct {
 
 func (limiter *RedisLimiter) RedisWaitAllowN(ctx context.Context, key string, limit redis_rate.Limit, n int) (bool, error) {
 	//如果同一时刻对同一个key传入不同的速率，会按照各自速率计算，并推进令牌桶的时间戳tat
-	res, err := limiter.AllowN(ctx, key, limit, n)
+	result, err := limiter.AllowN(ctx, key, limit, n)
 	if err != nil {
 		return false, err
 	}
-	if res.Allowed > 0 {
+	if result.Allowed > 0 {
 		//成功返回
 		return true, nil
 	}
-
+	//重试时间
+	resetAfter := result.ResetAfter
 	for {
 		select {
-		case <-time.After(res.ResetAfter):
-			res, err := limiter.AllowN(ctx, key, limit, n)
+		case <-time.After(resetAfter):
+			result, err := limiter.AllowN(ctx, key, limit, n)
 			if err != nil {
 				return false, err
 			}
-			if res.Allowed > 0 {
+			if result.Allowed > 0 {
 				//成功返回
 				return true, nil
 			}
+			resetAfter = result.ResetAfter
 		case <-ctx.Done():
 			//超时返回
 			return false, ctx.Err()
--- limiter/redis_limiter/redis_limiter_test.go
+++ limiter/redis_limiter/redis_limiter_test.go
@@ -14,12 +14,12 @@ import (
 func TestRedisLimiter_RedisWaitAllowN(t *testing.T) {
 	var globalwg sync.WaitGroup
 	globalwg.Add(2)
-	go RrunTest("test1", &globalwg)
-	go RrunTest("test2", &globalwg)
+	go RunTest("test1", &globalwg)
+	go RunTest("test2", &globalwg)
 	globalwg.Wait()
 }
 
-func RrunTest(key string, wg1 *sync.WaitGroup) {
+func RunTest(key string, wg1 *sync.WaitGroup) {
 	defer wg1.Done()
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("Redis_Addr"),
@@ -29,24 +29,24 @@ func RrunTest(key string, wg1 *sync.WaitGroup) {
 	limiters := RedisLimiter{Limiter: *redis_rate.NewLimiter(rdb)}
 	chatBots := []string{"bot1"}
 	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
 		for _, chatBot := range chatBots {
 			wg.Add(1)
 			go func(chatBot string, i int) {
 				defer wg.Done()
 				//var err error
 				startTime := time.Now()
-				withTimeout, cancelFunc := context.WithTimeout(context.Background(), 100*time.Second)
+				withTimeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
 				defer cancelFunc()
-				ok, err := limiters.RedisWaitAllowN(withTimeout, chatBot, redis_rate.PerSecond(6), 2)
+				ok, err := limiters.RedisWaitAllowN(withTimeout, chatBot, redis_rate.PerSecond(3), 2)
 				if err != nil {
 					fmt.Printf("%d限流器等待获取令牌失败, chatBotName=%s,等待时间%v error=%v\n", i, chatBot, time.Since(startTime), err)
 					return
 				}
 				if ok {
-					fmt.Printf("成功时间%v\n", time.Now())
+					fmt.Printf("%v成功时间%v\n", key, time.Now())
 				}
-				fmt.Printf("%schatBotName=%s 限流器%d等待时间%v等待结果%v, \n", key, chatBot, i, time.Since(startTime), ok)
+				//fmt.Printf("%schatBotName=%s 限流器%d等待时间%v等待结果%v, \n", key, chatBot, i, time.Since(startTime), ok)
 
 			}(chatBot, i)
 			//time.Sleep(10 * time.Millisecond) // 增加请求间隔，避免过多请求立即消耗掉令牌
